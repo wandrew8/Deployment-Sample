aed34836571c8468dccd1ef55b8c3203dd3cd860
Joel Detweiler <joeldd@uw.edu>
2020-08-23 20:30:12 -0700

    Add base route

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,14 @@ const { Router } = require("express");
 const router = Router();
 
 router.use("/items", require('./items'));
+router.get("/", (req, res, next) => {
+  res.send(`
+    <html>
+      <body>
+        <h1> Hello! </h1>
+      </body>
+    </html>
+  `)
+});
 
 module.exports = router;

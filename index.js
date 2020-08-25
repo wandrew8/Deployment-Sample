2dcc593de2d233624e33fe319d3d1ae0a15cb6e4
Joel Detweiler <joeldd@uw.edu>
2020-08-24 19:41:36 -0700

    Dynamic mongo connection

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const server = require("./server");
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const port = process.env.PORT || 5000;
 
-// mongoose.connect('mongodb://localhost/deployment-sample', {
-//   useNewUrlParser: true, 
-//   useCreateIndex: true, 
-//   useUnifiedTopology: true
-// }).then(() => {
-//   server.listen(port, () => {
-//    console.log(`Server is listening on http://localhost:${port}`);
-//   });
-// });
-
-server.listen(port, () => {
-  console.log(`Server is listening on http://localhost:${port}`);
- });
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+  useNewUrlParser: true, 
+  useCreateIndex: true, 
+  useUnifiedTopology: true
+}).then(() => {
+  server.listen(port, () => {
+   console.log(`Server is listening on http://localhost:${port}`);
+  });
+});

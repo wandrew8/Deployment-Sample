cdb744aa42830413f65a367ee6cbbd00ec344351
Andrew Weiss <wandrew8@gmail.com>
2020-08-30 18:31:41 -0700

    mongoose connection removed

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ dotenv.config();
 
 const port = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
-  useNewUrlParser: true, 
-  useCreateIndex: true, 
-  useUnifiedTopology: true
-}).then(() => {
-  server.listen(port, () => {
-   console.log(`Server is listening on http://localhost:${port}`);
-  });
-});
+// mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+//   useNewUrlParser: true, 
+//   useCreateIndex: true, 
+//   useUnifiedTopology: true
+// }).then(() => {
+//   server.listen(port, () => {
+//    console.log(`Server is listening on http://localhost:${port}`);
+//   });
+// });
+
+server.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}`);
+ });
